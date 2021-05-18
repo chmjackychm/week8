aeb67607b882ee7fd413cc1e39eea43855cd5f6d
chmjackychm <chmchenhaomingchen@gmail.com>
2021-05-18 15:11:20 +0000

    class example

--- index.js
+++ index.js
@@ -1,27 +1,80 @@
-document.addEventListener(`DOMContentLoaded`, async function(event) {
+firebase.auth().onAuthStateChanged(async function(user) {
+
+// document.addEventListener(`DOMContentLoaded`, async function(event) {
+if (user) {
+  // logged-in
+
+    // Build the markup for the sign-out button and set the HTML in the header
+    document.querySelector(`.sign-in-or-sign-out`).innerHTML = `
+    <button class="text-pink-500 underline sign-out">Sign Out</button>
+  `
+
+  // get a reference to the sign out button
+  let signOutButton = document.querySelector(`.sign-out`)
+
+  // handle the sign out button click
+  signOutButton.addEventListener(`click`, function(event) {
+    // sign out of firebase authentication
+    firebase.auth().signOut()
+
+    // redirect to the home page
+    document.location.href = `index.html`
+  })
   // Build the URL for our posts API
-
+   let url = `/.netlify/function/posts`
   // Fetch the url, wait for a response, store the response in memory
-
+  let response = await fetch(url)
   // Ask for the json-formatted data from the response, wait for the data, store it in memory
-
+  let json = await response.json()
   // Write the json-formatted data to the console in Chrome
+  console.log(json)
 
   // Grab a reference to the element with class name "posts" in memory
-
+  let postsDiv = document.querySelector(`.posts`)
   // Loop through the JSON data, for each Object representing a post:
-    // Store each object ("post") in memory
+  for (let i =0; i < json.length; i++) {  
+  // Store each object ("post") in memory
+  let post = json[i]
+
+  // Practice... how would you do the comments?
+  let commentMarkup = ``
+    // Loop through comments
+  let comments = post.comments
+  for (let i=0; i<comments.length; i++) {
+    let comment = comments[i]
+    commentMarkup = commentMarkup + `<div><strong>${comment.userName}</strong> ${comment.body}</div>`
+  }
     // Create some markup using the post data, insert into the "posts" element
     // e.g.
-    // <div class="md:mt-16 mt-8">
-    //   <div class="md:mx-0 mx-4 mt-8">
-    //     <span class="font-bold text-xl">Anonymous User</span>
-    //   </div>
-    //   <div class="my-8">
-    //     <img src="https://images.unsplash.com/..." class="w-full">
-    //   </div>
-    //   <!-- comments -->
-    //   <div><strong>Anonymous User</strong> Tacos for breakfast</div>
-    // </div>
-    // Practice... how would you do the comments?
+    postsDiv.insertAdjacentHTML(`beforeend`,
+    `<div class="md:mt-16 mt-8">
+      <div class="md:mx-0 mx-4 mt-8">
+        <span class="font-bold text-xl">${post.userName}</span>
+      </div>
+      <div class="my-8">
+        <img src=${post.imageUrl} class="w-full">
+      </div>
+      ${commentsMarkup}
+    </div>`)
+  }
+  } else {
+    // not logged in
+    console.log(`Not logged-in!`)
+
+    // user is not logged-in, so show login
+    // Initializes FirebaseUI Auth
+    let ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+    // FirebaseUI configuration
+    let authUIConfig = {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      signInSuccessUrl: `index.html` // where to go after we're done signing up/in
+    }
+
+    // Starts FirebaseUI Auth
+    ui.start(`.sign-in-or-sign-out`, authUIConfig)
+
+  }
 })
--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -2,12 +2,12 @@ const firebase = require("firebase/app")
 require("firebase/firestore")
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGd7OAVUT1u3Sf9uoSh_6l8iHg2qh_JJ4",
-  authDomain: "kiei-451-spring-2021.firebaseapp.com",
-  projectId: "kiei-451-spring-2021",
-  storageBucket: "kiei-451-spring-2021.appspot.com",
-  messagingSenderId: "902466001150",
-  appId: "1:902466001150:web:b5a42e97ab21026b706daf"
+  apiKey: "AIzaSyCgAFA0MNefdgbfvWj7aA0eFzTNf4bcZdg",
+  authDomain: "kiei-451-jc-bf473.firebaseapp.com",
+  projectId: "kiei-451-jc-bf473",
+  storageBucket: "kiei-451-jc-bf473.appspot.com",
+  messagingSenderId: "1048254492895",
+  appId: "1:1048254492895:web:a99fe60be345fe76bff96a"
 } // replace
 
 if (!firebase.apps.length) {
